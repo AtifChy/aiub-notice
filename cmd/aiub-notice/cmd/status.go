00c3f7c75eab9e031a6562ac9205a3517a5c3a7b
Atif <iftakhar.awal@gmail.com>
2025-09-03 00:41:24 +0600

    feat(cmd): add status command to check service state
    - implement status command to report if service is running
    - integrate with service process lock for status detection
    - update command registration in initialization

---
+++ cmd/aiub-notice/cmd/status.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/AtifChy/aiub-notice/internal/service"
+)
+
+// statusCmd represents the status command
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Check the status of the AIUB Notice Fetcher service",
+	Long:  `This command checks whether the AIUB Notice Fetcher service is currently running or not.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var isRunning bool
+		proc, _ := service.GetProcessFromLock()
+		if proc != nil {
+			isRunning = true
+		}
+		fmt.Printf("AIUB Notice Fetcher service is currently %s.\n", map[bool]string{true: "running", false: "not running"}[isRunning])
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
